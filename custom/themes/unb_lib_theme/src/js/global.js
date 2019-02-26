5bec88f807dfde37707cdfec2aff9bf1b092186c
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-02-26 08:47:45 -0400

    LIB-3 Disable affix for medium or less width devices

--- custom/themes/unb_lib_theme/src/js/global.js
+++ custom/themes/unb_lib_theme/src/js/global.js
@@ -10,10 +10,10 @@
   Drupal.behaviors.unb_lib_theme = {
     attach: function (context, settings) {
       var header = $("#navbar-main");
-      $(window).scroll(function() {
+      $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-
-        if (scroll >= 150) {
+        var width = $(window).innerWidth();
+        if (scroll >= 130 && width >= 991) {
           header.addClass("affix");
         } else {
           header.removeClass("affix");
@@ -21,5 +21,4 @@
       });
     }
   };
-
 })(jQuery, Drupal);
