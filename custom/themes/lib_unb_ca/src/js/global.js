950d259140f52b7e01f4631b7799103243e9de9c
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-03-15 10:18:04 -0300

    LIB-23 Set up initial subtheme js library, add affix transition

---
+++ custom/themes/lib_unb_ca/src/js/global.js
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * Global lib_unb_ca utilities.
+ *
+ */
+(function ($, Drupal) {
+
+    'use strict';
+
+    Drupal.behaviors.lib_unb_ca = {
+        attach: function (context, settings) {
+            var header_logo = $("#navbar-main .branding > a");
+            var header_nav_buttons = $("#navbar-main .megamenu-li > a");
+
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
+                var width = $(window).innerWidth();
+                if (scroll >= 130 && width >= 991) {
+                    header_nav_buttons.removeClass("py-lg-4");
+                    header_logo.removeClass("py-2");
+                } else {
+                    header_nav_buttons.addClass("py-lg-4");
+                    header_logo.addClass("py-2");
+
+                }
+            });
+        }
+    };
+})(jQuery, Drupal);
