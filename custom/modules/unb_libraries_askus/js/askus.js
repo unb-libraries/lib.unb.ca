d7ed6da2517b4afb9b6667b2531f1dfa3812fc48
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-05-08 10:01:15 -0300

    LIB-52 Add file comment, coding standards

--- custom/modules/unb_libraries_askus/js/askus.js
+++ custom/modules/unb_libraries_askus/js/askus.js
@@ -1,12 +1,18 @@
-(function($) {
-    'use strict';
+/**
+ * @file
+ * Drupal behavior to attach AskUs script.
+ * Source: https://docs.libraryh3lp.com/html-code-samples.
+ */
 
+(function ($, Drupal) {
     Drupal.behaviors.AskUs = {
-        attach: function ( context, settings ) {
-            // Source: https://docs.libraryh3lp.com/html-code-samples.
-            var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+        attach: function (context, settings) {
+            var x = document.createElement("script");
+            x.type = "text/javascript";
+            x.async = true;
             x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?multi";
-            var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-        },
+            var y = document.getElementsByTagName("script")[0];
+            y.parentNode.insertBefore(x, y);
+        }
     };
-})(jQuery);
+})(jQuery, Drupal);
