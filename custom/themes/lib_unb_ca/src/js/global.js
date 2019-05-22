2f8473a1edd2221295a74e460887a4444157c750
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-05-22 13:16:55 -0300

    LIB-73 Delete deprecated js behavior (moved to base theme)

--- custom/themes/lib_unb_ca/src/js/global.js
+++ custom/themes/lib_unb_ca/src/js/global.js
@@ -9,21 +9,7 @@
 
     Drupal.behaviors.lib_unb_ca = {
         attach: function (context, settings) {
-            var header_logo = $("#navbar-main .branding > *");
-            var header_nav_buttons = $("#navbar-main .megamenu-li > a");
-
-            $(window).scroll(function () {
-                var scroll = $(window).scrollTop();
-                var width = $(window).innerWidth();
-                if (scroll >= 130 && width >= 991) {
-                    header_nav_buttons.removeClass("py-lg-4");
-                    header_logo.removeClass("py-2");
-                } else {
-                    header_nav_buttons.addClass("py-lg-4");
-                    header_logo.addClass("py-2");
-
-                }
-            });
+            // Custom lib_unb_va Drupal Javascript Behaviors.
         }
     };
 })(jQuery, Drupal);
