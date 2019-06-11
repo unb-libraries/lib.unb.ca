fddf7b7c40fc707a21c64c7f0bce669e129eaf75
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-06-11 17:15:11 -0300

    LIB-99 Port get_guide_categories, use for  Research by Subjects render

--- custom/modules/lib_core/src/Plugin/Block/ResearchSubjects.php
+++ custom/modules/lib_core/src/Plugin/Block/ResearchSubjects.php
@@ -19,62 +19,44 @@ class ResearchSubjects extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $html =
-      '<h2>Research by Subject</h2> 
-      <div class="d-flex flex-wrap">
-        <ul class="flex-grow-1 mb-0">
-          <li><a href="//guides.lib.unb.ca/category/anthropology">Anthropology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/biology">Biology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/business">Business Administration</a></li>
-          <li><a href="//guides.lib.unb.ca/category/chemistry">Chemistry</a></li>
-          <li><a href="//guides.lib.unb.ca/category/classics">Classics and Ancient History</a></li>
-          <li><a href="//guides.lib.unb.ca/category/ics">Communication Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/computerscience">Computer Science</a></li>
-          <li><a href="//guides.lib.unb.ca/category/criminology">Criminology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/earth-sciences">Earth Sciences</a></li>
-          <li><a href="//guides.lib.unb.ca/category/economics">Economics</a></li>
-          <li><a href="//guides.lib.unb.ca/category/education">Education</a></li>
-          <li><a href="//guides.lib.unb.ca/category/engineering">Engineering</a></li>
-          <li><a href="//guides.lib.unb.ca/category/english">English</a></li>
-          <li><a href="//guides.lib.unb.ca/category/entrepreneurship">Entrepreneurship</a></li>
-        </ul>
-        <ul class="flex-grow-1 mb-0">
-          <li><a href="//guides.lib.unb.ca/category/familyviolence">Family Violence Issues</a></li>
-          <li><a href="//guides.lib.unb.ca/category/film">Film Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/finearts">Fine Arts</a></li>
-          <li><a href="//guides.lib.unb.ca/category/forestry">Forestry &amp; Environmental Management</a></li>
-          <li><a href="//guides.lib.unb.ca/category/french">French</a></li>
-          <li><a href="//guides.lib.unb.ca/category/womensstudies">Gender and Women\'s Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/german">German Literature</a></li>
-          <li><a href="//guides.lib.unb.ca/category/gerontology">Gerontology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/history">History</a></li>
-          <li><a href="//guides.lib.unb.ca/category/development">International Development Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/journalism">Journalism &amp; Communications</a></li>
-          <li><a href="//guides.lib.unb.ca/category/kinesiology">Kinesiology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/law">Law</a></li>
-          <li><a href="//guides.lib.unb.ca/category/math">Mathematics</a></li>
-        </ul>
-        <ul class="flex-grow-1">
-          <li><a href="//guides.lib.unb.ca/category/media">Media Arts and Culture</a></li>
-          <li><a href="//guides.lib.unb.ca/category/music">Music</a></li>
-          <li><a href="//guides.lib.unb.ca/category/nativestudies">Native Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/nursing">Nursing &amp; Health Sciences</a></li>
-          <li><a href="//guides.lib.unb.ca/category/philosophy">Philosophy</a></li>
-          <li><a href="//guides.lib.unb.ca/category/physics">Physics</a></li>
-          <li><a href="//guides.lib.unb.ca/category/politicalscience">Political Science</a></li>
-          <li><a href="//guides.lib.unb.ca/category/psychology">Psychology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/religiousstudies">Religious Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/sts">Science &amp; Technology Studies</a></li>
-          <li><a href="//guides.lib.unb.ca/category/socialwork">Social Work</a></li>
-          <li><a href="//guides.lib.unb.ca/category/sociology">Sociology</a></li>
-          <li><a href="//guides.lib.unb.ca/category/spanish">Spanish Literature</a></li>
-          <li class="list-unstyled mt-2">
-            <a href="//guides.lib.unb.ca/research-guides">
-              <span class="fa-plus-circle fas"></span>&nbsp;All Subject and Course Guides
-            </a>
-          </li>
-        </ul>
-      </div>';
+    $categories = _lib_core_get_guide_categories();
+    $col_list_item = 1;
+    $col_list_items = ceil(count($categories) / 3);
+    $last_item_label = end($categories);
+
+    $html = '<h2>Research by Subject</h2>';
+    $html .= '<div class="d-flex flex-wrap">';
+    $last_list_item_html =
+      '<li class="list-unstyled mt-2">
+        <a href="//guides.lib.unb.ca/research-guides">
+          <span class="fa-plus-circle fas"></span>&nbsp;All Subject and Course Guides
+        </a>
+      </li>
+      </ul>';
+
+    foreach ($categories as $value => $label) {
+      if ($col_list_item == 1) {
+        $html .= '<ul class="flex-grow-1 mb-0">';
+      }
+
+      $html .= '<li><a href="//guides.lib.unb.ca/category/' . $value . '">' . $label . '</a></li>';
+      if ($col_list_item < $col_list_items) {
+        if ($label === $last_item_label) {
+          $html .= $last_list_item_html;
+        }
+        else {
+          $col_list_item++;
+        }
+      }
+      elseif ($label === $last_item_label) {
+        $html .= $last_list_item_html;
+      }
+      else {
+        $col_list_item = 1;
+        $html .= '</ul>';
+      }
+    }
+    $html .= '</div>';
 
     $render_array['wrapper'] = [
       '#type' => 'container',
