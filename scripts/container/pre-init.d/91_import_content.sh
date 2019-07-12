7fc031a73ab6a3689828041dad97fceb82135747
jsanford <jsanford@unb.ca>
2019-07-12 13:24:39 -0300

    LIB-97 Import users, pages, news on startup

--- scripts/container/pre-init.d/91_import_content.sh
+++ scripts/container/pre-init.d/91_import_content.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env sh
-# Import content on restart.
+# Import content.
 if [[ "$IMPORT_INITIAL_CONTENT" == "TRUE" ]]; then
   cd /app/html
-  composer require drupal/migrate_plus drupal/migration_tools drupal/migrate_tools querypath/querypath
-  $DRUSH en lib_unb_ca_pages
+  # Install prerequisite modules for import.
+  composer require drupal/migrate_plus drupal/migration_tools drupal/migrate_tools querypath/querypath drupal/migrate_source_csv
+
+  # Ensure we query the proper host for content
+  echo "131.202.38.2    lib.unb.ca" >> /etc/hosts
+
+  # Run migrations.
+  $DRUSH en lib_unb_ca_users lib_unb_ca_pages lib_unb_ca_news
+  $DRUSH migrate-import lib_unb_ca_users
   $DRUSH migrate-import lib_unb_pages
+  $DRUSH migrate-import lib_unb_ca_news
   $DRUSH pmu lib_unb_ca_pages migrate migrate_plus migrate_tools migration_tools
-  composer remove drupal/migrate_plus drupal/migration_tools drupal/migrate_tools querypath/querypath
+
+  # Remove host entry
+  sed -i '/131.202/d' /etc/hosts
+
+  # Remove modules added for import only.
+  composer remove drupal/migrate_plus drupal/migration_tools drupal/migrate_tools querypath/querypath drupal/migrate_source_csv
   $DRUSH cr
 fi
