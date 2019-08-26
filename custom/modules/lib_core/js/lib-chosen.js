31f20b04dadf7ed54450d4cd50232e21107e7213
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-08-26 10:54:32 -0300

    LIB-120 Port mobile exceptions from chosen.jquery.js

--- custom/modules/lib_core/js/lib-chosen.js
+++ custom/modules/lib_core/js/lib-chosen.js
@@ -1,6 +1,13 @@
 (function ($, Drupal) {
     Drupal.behaviors.libChosen = {
         attach: function(context, settings) {
+            // Do not apply on Mobile: match chosen.jquery.js v.1.8.7 exceptions.
+            if (/iP(od|hone)/i.test(window.navigator.userAgent) || /IEMobile/i.test(window.navigator.userAgent) ||
+                /Windows Phone/i.test(window.navigator.userAgent) || /BlackBerry/i.test(window.navigator.userAgent) ||
+                /BB10/i.test(window.navigator.userAgent) || /Android.*Mobile/i.test(window.navigator.userAgent)) {
+                return false;
+            }
+
             // Invoke chosen() on main content select tags with options.
             // More options: https://harvesthq.github.io/chosen/options.html.
             $("select.chosen-select").chosen({
@@ -8,6 +15,7 @@
             });
             // Apply bootstrap 4 form-control class to chosen div.
             $(".chosen-container-single").addClass("form-control");
+
             // Resolve duplicate Discovery Search selects.
             $("select.chosen-select").hide();
         }
