bd6d7fbcd46e1299d3e205c7e4e2579f90e0981d
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-09-12 12:32:00 -0300

    LIB-120 Organize/clean up chosen-bootstrap library

--- custom/modules/lib_core/js/lib-chosen.js
+++ custom/modules/lib_core/js/lib-chosen.js
@@ -9,13 +9,12 @@
             }
 
             // Invoke chosen() on main content select tags with options.
-            // More options: https://harvesthq.github.io/chosen/options.html.
+            // See: https://harvesthq.github.io/chosen/options.html.
             $("select.chosen-select").chosen({
                 width: "100%"
             });
             // Apply bootstrap 4 form-control class to chosen div.
             $(".chosen-container-single").addClass("form-control");
-
             // Resolve duplicate Discovery Search selects.
             $("select.chosen-select").hide();
         }
