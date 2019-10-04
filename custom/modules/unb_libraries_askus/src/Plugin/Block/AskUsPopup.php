aafbc45a7d5a0f94139dbe64c0c338213a5faf95
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2019-10-04 14:58:37 -0300

    LIB-52 Apply link-no-hover to ask-us popup help link

--- custom/modules/unb_libraries_askus/src/Plugin/Block/AskUsPopup.php
+++ custom/modules/unb_libraries_askus/src/Plugin/Block/AskUsPopup.php
@@ -23,7 +23,7 @@ class AskUsPopup extends BlockBase {
     $chat_widget = _unb_libraries_askus_get_widget('popup');
     $chat_footer =
       '<p class="askus-footer text-center">
-        <a class="askus-help" href="/help/ask-us"><span class="sr-only">Ask by:</span>
+        <a class="askus-help link-no-hover" href="/help/ask-us"><span class="sr-only">Ask by:</span>
           <span><i class="fas fa-envelope"></i> Email</span>
           <span><i class="fas fa-sms"></i> Text</span>
           <span><i class="fas fa-phone-alt"></i> Phone</span>
