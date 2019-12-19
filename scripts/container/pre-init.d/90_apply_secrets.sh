317b808d42b811a76051a53fb87f4b14f2a033f3
Brian Cassidy <brian.cassidy@unb.ca>
2019-12-19 15:40:20 -0400

    escape slashes in variables

--- scripts/container/pre-init.d/90_apply_secrets.sh
+++ scripts/container/pre-init.d/90_apply_secrets.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 # OCLC API
+OCLC_API_WSKEY=$(echo "$OCLC_API_WSKEY" | sed 's/\//\\\//g')
+OCLC_API_SECRET=$(echo "$OCLC_API_SECRET" | sed 's/\//\\\//g')
 SETTINGS_FILE='/app/html/sites/all/settings/settings.oclc-api.inc'
 sed -i "s|OCLC_API_WSKEY|$OCLC_API_WSKEY|g" $SETTINGS_FILE
 sed -i "s|OCLC_API_SECRET|$OCLC_API_SECRET|g" $SETTINGS_FILE
