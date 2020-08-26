badf808c865d2c5970ee9edc7f1e9fbffb67d987
Brian Cassidy <brian.cassidy@unb.ca>
2020-08-26 15:27:24 -0300

    LIB-350 add secrets for OCLC IDM API

--- build/scripts/container/pre-init.d/90_apply_secrets.sh
+++ build/scripts/container/pre-init.d/90_apply_secrets.sh
@@ -3,6 +3,10 @@
 # OCLC API
 OCLC_API_WSKEY=$(echo "$OCLC_API_WSKEY" | sed 's/\//\\\//g')
 OCLC_API_SECRET=$(echo "$OCLC_API_SECRET" | sed 's/\//\\\//g')
+OCLC_API_IDM_WSKEY=$(echo "$OCLC_API_IDM_WSKEY" | sed 's/\//\\\//g')
+OCLC_API_IDM_SECRET=$(echo "$OCLC_API_IDM_SECRET" | sed 's/\//\\\//g')
 SETTINGS_FILE='/app/html/sites/all/settings/settings.oclc-api.inc'
 sed -i "s|OCLC_API_WSKEY|$OCLC_API_WSKEY|g" $SETTINGS_FILE
 sed -i "s|OCLC_API_SECRET|$OCLC_API_SECRET|g" $SETTINGS_FILE
+sed -i "s|OCLC_API_IDM_WSKEY|$OCLC_API_IDM_WSKEY|g" $SETTINGS_FILE
+sed -i "s|OCLC_API_IDM_SECRET|$OCLC_API_IDM_SECRET|g" $SETTINGS_FILE
