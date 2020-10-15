2d0f9435527e6e08317675a00e138522b35392be
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2020-10-15 10:07:45 -0300

    LIB-351 Separate Booking button from Quicklinks list, adjust responsive behaviour

--- custom/modules/lib_core/src/Plugin/Block/QuickLinks.php
+++ custom/modules/lib_core/src/Plugin/Block/QuickLinks.php
@@ -25,20 +25,16 @@ class QuickLinks extends BlockBase {
     $quicklinks = [
       [
         '#wrapper_attributes' => [
-          'class' => [
-            'list-inline-item',
-          ],
+          'class' => [],
         ],
         '#children' => Link::fromTextAndUrl(
-            $this->t('Renew Books'),
-            Url::fromUri('https://unb.on.worldcat.org/myaccount')
+          $this->t('Renew Books'),
+          Url::fromUri('https://unb.on.worldcat.org/myaccount')
         )->toString(),
       ],
       [
         '#wrapper_attributes' => [
-          'class' => [
-            'list-inline-item',
-          ],
+          'class' => [],
         ],
         '#children' => Link::fromTextAndUrl(
           $this->t('Document Delivery'),
@@ -47,9 +43,7 @@ class QuickLinks extends BlockBase {
       ],
       [
         '#wrapper_attributes' => [
-          'class' => [
-            'list-inline-item',
-          ],
+          'class' => [],
         ],
         '#children' => Link::fromTextAndUrl(
           Markup::create('<span class="fas fa-undo-alt fa-sm mr-1"></span>' . $this->t('Recalls')),
@@ -69,24 +63,20 @@ class QuickLinks extends BlockBase {
       ],*/
       [
         '#wrapper_attributes' => [
-          'class' => [
-            'list-inline-item',
-          ],
+          'class' => [],
         ],
         '#children' => Link::fromTextAndUrl(
           $this->t('Floor Plans'),
-            Url::fromUri('https://lib.unb.ca/about/find-us')
+          Url::fromUri('https://lib.unb.ca/about/find-us')
         )->toString(),
       ],
       [
         '#wrapper_attributes' => [
-          'class' => [
-            'list-inline-item',
-          ],
+          'class' => [],
         ],
         '#children' => Link::fromTextAndUrl(
           $this->t('Zotero'),
-            Url::fromUri('https://guides.lib.unb.ca/guide/206')
+          Url::fromUri('https://guides.lib.unb.ca/guide/206')
         )->toString(),
       ],
       /*[
@@ -100,26 +90,28 @@ class QuickLinks extends BlockBase {
             Url::fromUri('https://lib.unb.ca/services/borrow-tech-tools')
         )->toString(),
       ],*/
-      [
-        '#wrapper_attributes' => [
-          'class' => [
-            'list-inline-item',
-          ],
+    ];
+    $booking_btn = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'ml-auto',
+          'mr-auto',
+          'mr-lg-0',
         ],
-        '#children' => Link::fromTextAndUrl(
-          Markup::create('<span class="fas fa-calendar-check mr-1"></span>' . $this->t('Book a Seat')),
-          Url::fromUri('https://lib.unb.ca/services/bookings', [
-            'attributes' => [
-              'class' => [
-                'btn',
-                'btn-secondary',
-                'btn-sm',
-                'text-white',
-              ],
-            ],
-          ])
-        )->toString(),
       ],
+      '#children' => Link::fromTextAndUrl(
+        Markup::create('<span class="fas fa-calendar-check mr-1"></span>' . t('Book a Seat')),
+        Url::fromUri('https://lib.unb.ca/services/bookings', [
+          'attributes' => [
+            'class' => [
+              'btn',
+              'btn-dark',
+              'text-nowrap',
+             ],
+          ],
+        ])
+      )->toString(),
     ];
 
     $attachments = [
@@ -129,21 +121,33 @@ class QuickLinks extends BlockBase {
     ];
 
     $render_array_list = [
-      '#title' => 'QuickLinks:',
-      '#theme' => 'item_list',
-      '#list_type' => 'ul',
-      '#context' => [
-        'list_style' => 'inline',
-      ],
-      '#wrapper_attributes' => [
-        'id' => 'quicklinks-wrapper',
-        'class' => [
-          'collapse',
-          'mb-2',
+      '#type' => 'markup',
+      '#prefix' => '<div class="d-flex my-1" id="quicklinks">',
+      '#suffix' => '</div>',
+
+      'quicklinks_list' => [
+        '#title' => 'QuickLinks:',
+        '#attributes' => [
+          'class' => [
+            'd-md-inline-block',
+          ]
         ],
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+        '#context' => [
+          'list_style' => 'inline',
+        ],
+        '#wrapper_attributes' => [
+          'id' => 'quicklinks-wrapper',
+          'class' => [
+            'collapse',
+            'mr-2',
+          ],
+        ],
+        '#items' => $quicklinks,
+        '#attached' => $attachments,
       ],
-      '#items' => $quicklinks,
-      '#attached' => $attachments,
+      'quicklinks_book' => $booking_btn,
     ];
 
     return $render_array_list;
