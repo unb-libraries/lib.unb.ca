e7b0fd28351678ed6bd33e2f88c39fb417cbf777
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-04-09 14:15:17 -0300

    BugzID: 14196 Switch from CSS to JS-based anchor/affix workaround

--- custom/themes/lib_unb_ca/src/js/global.js
+++ custom/themes/lib_unb_ca/src/js/global.js
@@ -1,15 +1,24 @@
 /**
  * @file
- * Global lib_unb_ca utilities.
- *
+ * Global lib_unb_ca JS functions.
+ * https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header/29853395#29853395.
  */
-(function ($, Drupal) {
+ (function($, window) {
+    var adjustAnchor = function() {
+        var $anchor = $(':target'), fixedElementHeight = 80;
 
-    'use strict';
-
-    Drupal.behaviors.lib_unb_ca = {
-        attach: function (context, settings) {
-            // Custom lib_unb_va Drupal Javascript Behaviors.
+        if ($anchor.length > 0) {
+            $('html, body')
+                .stop()
+                .animate({
+                    scrollTop: $anchor.offset().top - fixedElementHeight
+                }, 150);
         }
     };
-})(jQuery, Drupal);
+
+    $(window).on('hashchange load', function() {
+        adjustAnchor();
+    });
+
+ })(jQuery, window);
+
