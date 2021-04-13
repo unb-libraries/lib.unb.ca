c301f012dd28b3a05e000262272f30454621e0e2
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-04-13 11:17:41 -0300

    BugzID: 14196 Factor in conditional affix re: offset

--- custom/themes/lib_unb_ca/src/js/global.js
+++ custom/themes/lib_unb_ca/src/js/global.js
@@ -3,26 +3,27 @@
  * Global lib_unb_ca JS functions.
  * https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header/29853395#29853395.
  */
-(function ($, Drupal) {
+(function($, Drupal) {
 
-  'use strict';
+	'use strict';
 
-  Drupal.behaviors.lib_theme = {
-      attach: function (context, settings) {
-          var adjustAnchor = function () {
-              var $anchor = $(':target'), fixedElementHeight = 80;
-              if ($anchor.length > 0) {
-                  $('html, body')
-                      .stop()
-                      .animate({
-                          scrollTop: $anchor.offset().top - fixedElementHeight
-                      }, 150);
-              }
-          };
-
-          $(window).on('hashchange load pageshow', function () {
-              adjustAnchor();
-          });
-      }
-  };
+	Drupal.behaviors.lib_unb_ca = {
+		attach: function(context, settings) {
+			$(window).on('hashchange pageshow', function() {
+				adjustAnchor();
+			});
+		}
+	};
 })(jQuery, Drupal);
+
+var adjustAnchor = function() {
+	var $anchor = jQuery(':target');
+	if ($anchor.length > 0) {
+		var $fixedElementHeight = jQuery('#navbar-main.affix').length ? 80 : 160;
+		jQuery('html, body')
+			.stop()
+			.animate({
+				scrollTop: $anchor.offset().top - $fixedElementHeight
+            }, 150);
+    }
+};
