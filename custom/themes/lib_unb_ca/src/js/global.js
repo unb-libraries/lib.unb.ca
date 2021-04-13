2ea02ff80d66fd4f2a70a13ca53a3cf779232744
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-04-13 14:43:34 -0300

    Code indentation

--- custom/themes/lib_unb_ca/src/js/global.js
+++ custom/themes/lib_unb_ca/src/js/global.js
@@ -4,15 +4,15 @@
  */
 (function($, Drupal) {
 
-	'use strict';
+    'use strict';
 
-	Drupal.behaviors.lib_unb_ca = {
-		attach: function(context, settings) {
-			$(window).on('hashchange pageshow', function() {
-				adjustAnchor();
-			});
-		}
-	};
+    Drupal.behaviors.lib_unb_ca = {
+        attach: function(context, settings) {
+            $(window).on('hashchange pageshow', function() {
+                adjustAnchor();
+            });
+        }
+    };
 })(jQuery, Drupal);
 
 /**
@@ -20,12 +20,12 @@
  * https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header/29853395#29853395.
  */
 var adjustAnchor = function() {
-	var $anchor = jQuery(':target');
-	if ($anchor.length > 0) {
-		var $fixedElementHeight = jQuery('#navbar-main.affix').length ? 80 : 160;
-		jQuery('html, body')
-			.stop()
-			.animate({
+    var $anchor = jQuery(':target');
+    if ($anchor.length > 0) {
+        var $fixedElementHeight = jQuery('#navbar-main.affix').length ? 80 : 160;
+        jQuery('html, body')
+            .stop()
+            .animate({
 				scrollTop: $anchor.offset().top - $fixedElementHeight
             }, 150);
     }
