befb39267447325ae6112a22aadb038fe796ca60
Patrick Schilf <pschilf@unb.ca>
2021-05-18 17:27:17 -0300

    LIB-390 Install "abstract" field

--- custom/modules/portolan/src/Entity/PortolanRecord.php
+++ custom/modules/portolan/src/Entity/PortolanRecord.php
@@ -48,7 +48,12 @@ class PortolanRecord extends ContentEntityBase {
       ->setCardinality(1)
       ->setSetting('max_length', 1024);
 
-    // @todo Install "abstract" field [string(2048)]
+    $fields['abstract'] = BaseFieldDefinition::create('text')
+      ->setLabel(t('Abstract'))
+      ->setRequired(TRUE)
+      ->setCardinality(1)
+      ->setSetting('max_length', 2048);
+
     // @todo Install "note" field [string(1024)]
     // @todo Install "age_range" field [string(255)]
     // @todo Install "jurisdiction" field [multi-value, string(255)]
