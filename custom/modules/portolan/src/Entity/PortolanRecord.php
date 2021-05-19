0b8fa5e5e6ce758be1e4efcfd2b96c7465b71c2c
Patrick Schilf <patrick.schilf@unb.ca>
2021-05-19 16:29:07 -0300

    LIB-390 Install "descriptor" field

--- custom/modules/portolan/src/Entity/PortolanRecord.php
+++ custom/modules/portolan/src/Entity/PortolanRecord.php
@@ -76,6 +76,11 @@ class PortolanRecord extends ContentEntityBase {
       ->setCardinality(BaseFieldDefinition::CARDINALITY_UNLIMITED);
 
     // @todo Install "descriptors" field [multi-value, string(255)]
+    $fields['descriptor'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Descriptor(s)'))
+      ->setRequired(TRUE)
+      ->setCardinality(BaseFieldDefinition::CARDINALITY_UNLIMITED);
+
     // @todo Install "call_num" field [string(255)]
     return $fields;
   }
