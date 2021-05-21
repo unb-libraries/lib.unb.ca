13298ff51f27caf2d2a7744ff6fe91259550c336
Brian Cassidy <brian.cassidy@unb.ca>
2021-05-21 16:14:03 -0300

    add KB key

--- build/scripts/container/pre-init.d/90_apply_secrets.sh
+++ build/scripts/container/pre-init.d/90_apply_secrets.sh
@@ -21,6 +21,11 @@ SETTINGS_FILE='/app/keys/api.oclc-lman.json'
 sed -i "s|OCLC_API_LMAN_WSKEY|$OCLC_API_LMAN_WSKEY|g" $SETTINGS_FILE
 sed -i "s|OCLC_API_LMAN_SECRET|$OCLC_API_LMAN_SECRET|g" $SETTINGS_FILE
 
+# OCLC KB Key
+OCLC_API_KB=$(echo "$OCLC_API_KB" | sed 's/\//\\\//g')
+SETTINGS_FILE='/app/keys/api.knowledge_base.json'
+sed -i "s|OCLC_API_KB|$OCLC_API_KB|g" $SETTINGS_FILE
+
 # OLD STYLE
 SETTINGS_FILE='/app/html/sites/all/settings/settings.oclc-api.inc'
 sed -i "s|OCLC_API_WSKEY|$OCLC_API_WSKEY|g" $SETTINGS_FILE
