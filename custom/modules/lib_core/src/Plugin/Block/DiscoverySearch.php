978a05550ca5888f7b203a9c411b62f086e81be3
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-07-12 09:27:53 -0300

    LIB-407 Discovery search: add category heading by toggle btn, adjust BS4 classes

--- custom/modules/lib_core/src/Plugin/Block/DiscoverySearch.php
+++ custom/modules/lib_core/src/Plugin/Block/DiscoverySearch.php
@@ -41,29 +41,30 @@ class DiscoverySearch extends BlockBase {
       '<div class="Accordion d-flex flex-column flex-lg-row px-4 px-lg-0 theme-dark">
         <div id="discovery-search" class="flex-grow-1">
         <div class="card">
-          <h2 class="sr-only">Search</h2>
-          <div class="card-header px-2 pb-1">
+          <div class="card-header px-2 pb-0 pb-md-1">
+            <h2 class="sr-only">Search</h2>
             <nav class="navbar navbar-expand-md text-nowrap">
-            <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                Select a search category
-            </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                   <ul class="navbar-nav d-flex align-items-lg-end justify-content-between w-100">
-                      <li class="nav-item"><button aria-controls="searchPanel1" aria-expanded="true" class="Accordion-trigger p-2" id="searchBtn1">Catalogue</button></li>
-                      <li class="nav-item"><button aria-controls="searchPanel2" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn2" tabindex="-1">Reserves</button></li>
-                      <li class="nav-item"><button aria-controls="searchPanel3" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn3" tabindex="-1">Article Databases</button></li>
-                      <li class="nav-item"><button aria-controls="searchPanel4" aria-expanded="false" class="Accordion-trigger p-2 text-left text-lg-center" id="searchBtn4" tabindex="-1">Journals &amp; Newspapers</button></li>
-                      <li class="nav-item">
-                         <a class="btn Accordion-trigger p-2 rounded-0 shadow-none text-left text-lg-center text-nowrap"
-                            id="searchBtn5" aria-controls="searchPanel5" aria-expanded="false" tabindex="-1"
-                            href="https://web.lib.unb.ca/eresources"
-                            onclick="location.href=\'https://web.lib.unb.ca/eresources\';"
-                            title="Guide to Finding Reference Materials (opens new page)">More<i class="fas fa-angle-double-right fa-sm fa-muted ml-1" aria-hidden="true"></i>
-                         </a>
-                      </li>
-                    </ul>
-                </div>
-             </nav>
+              <h3 id="category-label" class="d-block d-md-none h4 px-2 py-1 text-warning">Catalogue</h3>
+              <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                Switch search
+              </button>
+              <div class="collapse mx-3 mx-lg-0 navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav d-flex align-items-lg-end justify-content-between w-100">
+                  <li class="nav-item"><button aria-controls="searchPanel1" aria-expanded="true" class="Accordion-trigger p-2" id="searchBtn1">Catalogue</button></li>
+                  <li class="nav-item"><button aria-controls="searchPanel2" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn2" tabindex="-1">Reserves</button></li>
+                  <li class="nav-item"><button aria-controls="searchPanel3" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn3" tabindex="-1">Article Databases</button></li>
+                  <li class="nav-item"><button aria-controls="searchPanel4" aria-expanded="false" class="Accordion-trigger p-2 text-left text-lg-center" id="searchBtn4" tabindex="-1">Journals &amp; Newspapers</button></li>
+                  <li class="nav-item">
+                    <a class="btn Accordion-trigger p-2 rounded-0 shadow-none text-left text-lg-center text-nowrap"
+                       id="searchBtn5" aria-controls="searchPanel5" aria-expanded="false" tabindex="-1"
+                       href="https://web.lib.unb.ca/eresources"
+                       onclick="location.href=\'https://web.lib.unb.ca/eresources\';"
+                       title="Guide to Finding Reference Materials (opens new page)">More<i class="fas fa-angle-double-right fa-sm fa-muted ml-1" aria-hidden="true"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
           </div>
           <div class="card-body px-2 py-0">
             <div aria-labelledby="searchBtn1 dropdownBtn1" class="Accordion-panel" id="searchPanel1" role="region">' . $this->getCatalogueForm() . '</div>
