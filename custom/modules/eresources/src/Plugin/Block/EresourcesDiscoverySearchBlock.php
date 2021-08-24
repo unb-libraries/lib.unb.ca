3d0054aaa9a914080eee5e0115c73eb48630f6ec
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-08-24 11:24:00 -0300

    LIB-421 Match tabbed container ids with web.lib (re: existing bookmarks)

--- custom/modules/eresources/src/Plugin/Block/EresourcesDiscoverySearchBlock.php
+++ custom/modules/eresources/src/Plugin/Block/EresourcesDiscoverySearchBlock.php
@@ -20,7 +20,7 @@ class EresourcesDiscoverySearchBlock extends BlockBase {
    */
   public function build() {
     $build = '
-     <div class="Accordion d-flex flex-column flex-lg-row px-4 px-lg-0">
+     <div class="Accordion d-flex flex-column flex-lg-row">
         <div id="eresources-discovery-search" class="flex-grow-1">
         <div class="card">
           <div class="card-header bg-black px-2 pb-0 pb-md-1">
@@ -32,22 +32,22 @@ class EresourcesDiscoverySearchBlock extends BlockBase {
               </button>
               <div class="collapse mx-3 mx-lg-0 navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav d-flex align-items-lg-end justify-content-between w-100">
-                  <li class="nav-item"><button aria-controls="searchPanel1" aria-expanded="true" class="Accordion-trigger p-2" id="searchBtn1">Article Databases</button></li>
-                  <li class="nav-item"><button aria-controls="searchPanel2" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn2" tabindex="-1">Journals</button></li>
-                  <li class="nav-item"><button aria-controls="searchPanel3" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn3" tabindex="-1">e-Reference Materials</button></li>
-                  <li class="nav-item"><button aria-controls="searchPanel4" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn4" tabindex="-1">eBooks</button></li>
-                  <li class="nav-item"><button aria-controls="searchPanel3" aria-expanded="false" class="Accordion-trigger p-2" id="searchBtn5" tabindex="-1">Videos</button></li>
+                  <li class="nav-item"><button aria-controls="indexes" aria-expanded="true" class="Accordion-trigger p-2" id="indexesBtn">Article Databases</button></li>
+                  <li class="nav-item"><button aria-controls="journals" aria-expanded="false" class="Accordion-trigger p-2" id="journalsBtn" tabindex="-1">Journals</button></li>
+                  <li class="nav-item"><button aria-controls="refmat" aria-expanded="false" class="Accordion-trigger p-2" id="refmatBtn" tabindex="-1">e-Reference Materials</button></li>
+                  <li class="nav-item"><button aria-controls="ebooks" aria-expanded="false" class="Accordion-trigger p-2" id="ebooksBtn" tabindex="-1">eBooks</button></li>
+                  <li class="nav-item"><button aria-controls="video" aria-expanded="false" class="Accordion-trigger p-2" id="videoBtn" tabindex="-1">Videos</button></li>
                   </li>
                 </ul>
               </div>
             </nav>
           </div>
           <div class="card-body bg-light px-2 py-0">
-            <div aria-labelledby="searchBtn1 dropdownBtn1" class="Accordion-panel" id="searchPanel1" role="region">' . $this->getBlockMarkup('databases_search_block') . '</div>
-            <div aria-labelledby="searchBtn2 dropdownBtn2" class="Accordion-panel" id="searchPanel2" role="region" hidden="">' . $this->getBlockMarkup('journals_search_block') . '</div>
-            <div aria-labelledby="searchBtn3 dropdownBtn3" class="Accordion-panel" id="searchPanel3" role="region" hidden="">' . $this->getBlockMarkup('reference_search_block') . '</div>
-            <div aria-labelledby="searchBtn4 dropdownBtn4" class="Accordion-panel" id="searchPanel4" role="region" hidden="">' . $this->getBlockMarkup('ebooks_search_block') . '</div>
-            <div aria-labelledby="searchBtn5 dropdownBtn5" class="Accordion-panel" id="searchPanel5" role="region" hidden="">' . $this->getBlockMarkup('videos_search_block') . '</div>
+            <div aria-labelledby="indexesBtn dropdownBtn1" class="Accordion-panel" id="indexes" role="region">' . $this->getBlockMarkup('databases_search_block') . '</div>
+            <div aria-labelledby="journalsBtn dropdownBtn2" class="Accordion-panel" id="journals" role="region" hidden="">' . $this->getBlockMarkup('journals_search_block') . '</div>
+            <div aria-labelledby="refmatBtn dropdownBtn3" class="Accordion-panel" id="refmat" role="region" hidden="">' . $this->getBlockMarkup('reference_search_block') . '</div>
+            <div aria-labelledby="ebooksBtn dropdownBtn4" class="Accordion-panel" id="ebooks" role="region" hidden="">' . $this->getBlockMarkup('ebooks_search_block') . '</div>
+            <div aria-labelledby="videoBtn dropdownBtn5" class="Accordion-panel" id="video" role="region" hidden="">' . $this->getBlockMarkup('videos_search_block') . '</div>
           </div>
         </div>
         </div>
