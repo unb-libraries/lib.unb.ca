098eff11922afe2b35d21a2868ceb3b96489aea3
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-08-26 16:01:17 -0300

    LIB-421 Add support switch 'tab' from search query

--- custom/modules/eresources/js/navbar.js
+++ custom/modules/eresources/js/navbar.js
@@ -1,14 +1,39 @@
 (function ($, Drupal, DrupalSettings) {
     Drupal.behaviors.eresources = {
         attach: function(context, settings) {
-             $(document, context).once('bookmarks').each( function() {
+            $(document, context).once('bookmarks').each( function() {
                 var queryString = window.location.search;
                 var urlParams = new URLSearchParams(queryString);
-                var sub = urlParams.get('sub');
-                if (sub) {
-                    var button = $("#eresources-discovery-search button[aria-controls='" + sub + "']");
+                var form_id = urlParams.get('form_id');
+                if (form_id) {
+                    switch (form_id) {
+                        case 'eresources_journals_form':
+                            var aria_controls = 'journals';
+                            break;
+                        case 'eresources_reference_form':
+                            var aria_controls = 'refmat';
+                            break;
+                        case 'eresources_ebooks_form':
+                            var aria_controls = 'ebooks';
+                            break;
+                        case 'eresources_videos_form':
+                            var aria_controls = 'video';
+                            break;
+                        default:
+                            var aria_controls = 'indexes';
+                    }
+                    var button = $("#eresources-discovery-search button[aria-controls='" + aria_controls + "']");
                     if (button) {
                         $(button).click().focus();
+                    }
+                } else {
+                    // Backwards support for web.lib | sub | url argument
+                    var sub = urlParams.get('sub');
+                    if (sub) {
+                        var button = $("#eresources-discovery-search button[aria-controls='" + sub + "']");
+                        if (button) {
+                            $(button).click().focus();
+                        }
                     }
                 }
             });
