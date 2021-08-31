4419e566b6cf3f991a769bd7d5457d2acc8a09fb
Brian Cassidy <brian.cassidy@unb.ca>
2021-08-31 13:19:38 -0300

    LIB-421 remove blocks and use form classes directly; add history js

--- custom/modules/eresources/js/navbar.js
+++ custom/modules/eresources/js/navbar.js
@@ -1,40 +1,36 @@
 (function ($, Drupal, DrupalSettings) {
     Drupal.behaviors.eresources = {
         attach: function(context, settings) {
-            $(document, context).once('bookmarks').each( function() {
-                var queryString = window.location.search;
-                var urlParams = new URLSearchParams(queryString);
-                var form_id = urlParams.get('form_id');
-                if (form_id) {
-                    switch (form_id) {
-                        case 'eresources_journals_form':
-                            var aria_controls = 'journals';
-                            break;
-                        case 'eresources_reference_form':
-                            var aria_controls = 'refmat';
-                            break;
-                        case 'eresources_ebooks_form':
-                            var aria_controls = 'ebooks';
-                            break;
-                        case 'eresources_videos_form':
-                            var aria_controls = 'video';
-                            break;
-                        default:
-                            var aria_controls = 'indexes';
-                    }
-                    var button = $("#eresources-discovery-search button[aria-controls='" + aria_controls + "']");
-                    if (button) {
-                        $(button).click().focus();
-                    }
-                } else {
-                    // Backwards support for web.lib | sub | url argument
-                    var sub = urlParams.get('sub');
-                    if (sub) {
-                        var button = $("#eresources-discovery-search button[aria-controls='" + sub + "']");
-                        if (button) {
-                            $(button).click().focus();
-                        }
-                    }
+            var location = window.location;
+            var url = location.href;
+            var urlParams = new URLSearchParams(location.search);
+            var formId = urlParams.get('form_id');
+            var original = formId ? formId : '';
+
+            // Add history on tab change.
+            $('#navbarNav button').on('click', function() {
+                var href = $(this).attr('href');
+                if(original && href.includes(original)) {
+                    history.pushState({}, "", url);
+                }
+                else {
+                    history.pushState({}, "", href);
+                }
+            });
+
+            // Restore tab on back button.
+            window.addEventListener("popstate", function(e) {
+                var newUrlParams = new URLSearchParams(window.location.search);
+                var newFormId = newUrlParams.get('form_id');
+
+                // TODO: trigger tab via js.
+                // Existing form ID.
+                if(newFormId) {
+                  newFormId = newFormId.replace('eresources_', '');
+                  newFormId = newFormId.replace('_form', '');
+                }
+                // No form ID, trigger first tab.
+                else {
                 }
             });
         }
--- custom/modules/eresources/src/Plugin/Block/EresourcesDiscoverySearchBlock.php
+++ custom/modules/eresources/src/Plugin/Block/EresourcesDiscoverySearchBlock.php
@@ -19,6 +19,21 @@ class EresourcesDiscoverySearchBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $formBuilder = \Drupal::formBuilder();
+    $renderer = \Drupal::service('renderer');
+    $formId = \Drupal::request()->get('form_id');
+    if (empty($formId)) {
+      $formId = 'databases';
+    }
+
+    $forms = [
+      'databases' => 'Article Databases',
+      'journals' => 'Journals',
+      'reference' => 'e-Reference Materials',
+      'ebooks' => 'eBooks',
+      'videos' => 'Videos',
+    ];
+
     $build = '
      <div class="Accordion d-flex flex-column flex-lg-row">
         <div id="eresources-discovery-search" class="flex-grow-1">
@@ -32,22 +47,28 @@ class EresourcesDiscoverySearchBlock extends BlockBase {
               </button>
               <div class="collapse mx-3 mx-lg-0 navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav d-flex align-items-lg-end justify-content-between w-100">
-                  <li class="nav-item"><button aria-controls="indexes" aria-expanded="true" class="Accordion-trigger p-2" id="indexesBtn">Article Databases</button></li>
-                  <li class="nav-item"><button aria-controls="journals" aria-expanded="false" class="Accordion-trigger p-2" id="journalsBtn" tabindex="-1">Journals</button></li>
-                  <li class="nav-item"><button aria-controls="refmat" aria-expanded="false" class="Accordion-trigger p-2" id="refmatBtn" tabindex="-1">e-Reference Materials</button></li>
-                  <li class="nav-item"><button aria-controls="ebooks" aria-expanded="false" class="Accordion-trigger p-2" id="ebooksBtn" tabindex="-1">eBooks</button></li>
-                  <li class="nav-item"><button aria-controls="video" aria-expanded="false" class="Accordion-trigger p-2" id="videoBtn" tabindex="-1">Videos</button></li>
-                  </li>
+    ';
+
+    foreach ($forms as $form => $title) {
+      $current = preg_match("/$form/", $formId) ? 'true' : 'false';
+      $build .= '<li class="nav-item"><button href="?form_id=eresources_' . $form . '_form" aria-controls="' . $form . '" aria-expanded="' . $current . '" class="Accordion-trigger p-2" id="' . $form . 'Btn">' . $title . '</button></li>';
+    }
+
+    $build .= '
                 </ul>
               </div>
             </nav>
           </div>
           <div class="card-body bg-light px-2 py-0">
-            <div aria-labelledby="indexesBtn dropdownBtn1" class="Accordion-panel" id="indexes" role="region">' . $this->getBlockMarkup('databases_search_block') . '</div>
-            <div aria-labelledby="journalsBtn dropdownBtn2" class="Accordion-panel" id="journals" role="region" hidden="">' . $this->getBlockMarkup('journals_search_block') . '</div>
-            <div aria-labelledby="refmatBtn dropdownBtn3" class="Accordion-panel" id="refmat" role="region" hidden="">' . $this->getBlockMarkup('reference_search_block') . '</div>
-            <div aria-labelledby="ebooksBtn dropdownBtn4" class="Accordion-panel" id="ebooks" role="region" hidden="">' . $this->getBlockMarkup('ebooks_search_block') . '</div>
-            <div aria-labelledby="videoBtn dropdownBtn5" class="Accordion-panel" id="video" role="region" hidden="">' . $this->getBlockMarkup('videos_search_block') . '</div>
+    ';
+
+    foreach ($forms as $form => $title) {
+      $hidden = preg_match("/$form/", $formId) ? '' : 'hidden';
+      $formRender = $formBuilder->getForm('Drupal\eresources\Form\\' . ucfirst($form) . 'Form');
+      $build .= '<div aria-labelledby="' . $form . 'Btn" class="Accordion-panel" id="' . $form . '" role="region"' . $hidden . '>' . $renderer->render($formRender) . '</div>';
+    }
+
+    $build .= '
           </div>
         </div>
         </div>
@@ -63,25 +84,6 @@ class EresourcesDiscoverySearchBlock extends BlockBase {
         ],
       ],
     ];
-  }
-
-  /**
-   * Get HTML markup for a block plugin, given its plugin id.
-   *
-   * @param string $plugin_id
-   *   The plugin id of the desired block.
-   * @param array $configuration
-   *   The configuration array for the desired block plugin, default NULL.
-   *
-   * @return \Drupal\Core\Render\Markup
-   *   The rendered HTML markup for the block plugin ('broken/missing' message if invalid).
-   */
-  protected function getBlockMarkup($plugin_id, array $configuration = []) {
-    $block_manager = \Drupal::service('plugin.manager.block');
-    $plugin_block = $block_manager->createInstance($plugin_id, $configuration);
-    $render = $plugin_block->build();
-
-    return render($render);
   }
 
 }
