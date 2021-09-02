6136d942ff9f4627a431dd0b2e97f72c7dfaaf7b
Patrick Schilf <pschilf@unb.ca>
2021-09-02 10:48:59 -0300

    RECORDS-5 Install "Retention schedule" entity

---
+++ custom/modules/records_management/src/Entity/Schedule.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\records_management\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+
+/**
+ * The "Retention schedule" entity.
+ *
+ * @ContententityType(
+ *   id = "schedule",
+ *   label = @Translation("Retention schedule"),
+ *   label_plural = @Translation("Retention schedules"),
+ *   label_collection = @Translation("Retention schedules"),
+ *   handlers = {
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\DefaultHtmlRouteProvider"
+ *     }
+ *   },
+ *   base_table = "schedule",
+ *   revision_table = "schedule_revision",
+ *   admin_permission = "administer schedule entities",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "revision" = "rid",
+ *     "label" = "name",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "add-form" = "/records/schedules/add",
+ *     "edit-form" = "/records/schedules/{schedule}/edit",
+ *     "delete-form" = "/records/schedules/{schedule}/delete",
+ *     "collection" = "/records/schedules",
+ *   }
+ * )
+ */
+class Schedule extends ContentEntityBase implements ScheduleInterface {
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    // @todo Install required "name" string field.
+    // @todo Install required "number" string field.
+    // @todo Install required "classification" entity_reference (classification) field.
+    // @todo Install required "oopr" string_list field. Allowed values to be defined.
+    // @todo Install required "purpose" text field.
+    // @todo Install required "description" text field.
+    // @todo Install multi-value (max: 2) required "retention_details" entity_reference (retention_details) field.
+    // @todo Install optional "rationale" text field.
+    // @todo Install required "vital" bool field.
+    // @todo Install required "personal" bool field.
+    // @todo Install required "approved" date field.
+    // @todo Install required "last_revised" date field.
+    return $fields;
+  }
+
+}
---
+++ custom/modules/records_management/src/Entity/ScheduleInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\records_management\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Interface for "Retention schedule" entities.
+ *
+ * @package Drupal\records_management\Entity
+ */
+interface ScheduleInterface extends ContentEntityInterface {
+}
