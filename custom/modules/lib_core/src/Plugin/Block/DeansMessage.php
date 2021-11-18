5ce933c6973008bf4442e1a266a2d7d3a19388fc
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2021-11-17 21:40:20 -0400

    LIB-464 Remove border + background classes from the Dean block plugin

--- custom/modules/lib_core/src/Plugin/Block/DeansMessage.php
+++ custom/modules/lib_core/src/Plugin/Block/DeansMessage.php
@@ -23,10 +23,7 @@ class DeansMessage extends BlockBase {
       '#type' => 'html_tag',
       '#tag' => 'div',
       '#attributes' => [
-        'class' => [
-          'front-page-border',
-          'bg-light',
-        ],
+        'class' => [],
       ],
       '#value' => $this->getValue(),
     ];
