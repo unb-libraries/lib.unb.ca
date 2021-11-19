5cb53ee9010b254bd089a5bdffcaa85d0b0abcfd
Patrick Schilf <pschilf@unb.ca>
2021-11-19 12:59:33 -0400

    LIB-453 Display copyright/contest rules agreement checkbox

--- custom/modules/ior/src/Form/SubmissionForm.php
+++ custom/modules/ior/src/Form/SubmissionForm.php
@@ -35,6 +35,37 @@ class SubmissionForm extends ContentEntityForm {
   /**
    * {@inheritDoc}
    */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    if ($this->getEntity()->isNew()) {
+      $form['agree_copyright_1'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('I certify that I am the copyright owner of the submitted image and 
+have the necessary rights, permissions, and/or licenses to submit the 
+image to the competition according to the full contest rules and 
+conditions.'),
+        '#required' => TRUE,
+        '#default_value' => FALSE,
+        '#weight' => 97,
+      ];
+
+      $form['agree_copyright_2'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('I provide the university with the nonexclusive right to use the submitted 
+image while I remain the copyright holder of the image.'),
+        '#required' => TRUE,
+        '#default_value' => FALSE,
+        '#weight' => 98,
+      ];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   public function buildEntity(array $form, FormStateInterface $form_state) {
     $entity = parent::buildEntity($form, $form_state);
     $entity->setContest($this->getContest());
