468f6b07707d358fed42f1109c58dfcdce07be06
Patrick Schilf <pschilf@unb.ca>
2021-11-23 14:21:19 -0400

    LIB-453 Link to contest rules

--- custom/modules/ior/src/Form/SubmissionForm.php
+++ custom/modules/ior/src/Form/SubmissionForm.php
@@ -4,6 +4,8 @@ namespace Drupal\ior\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 
 /**
  * Default form for "submission" entities.
@@ -32,7 +34,9 @@ class SubmissionForm extends ContentEntityForm {
     if ($this->getEntity()->isNew()) {
       $form['agree_contest_rules'] = [
         '#type' => 'checkbox',
-        '#title' => $this->t('I certify that I am the copyright owner of the submitted image and have the necessary rights, permissions, and/or licenses to submit the image to the competition according to the full contest rules and conditions.'),
+        '#title' => $this->t('I certify that I am the copyright owner of the submitted image and have the necessary rights, permissions, and/or licenses to submit the image to the competition according to the full @link_to_contest_rules.', [
+          '@link_to_contest_rules' => $this->buildContestRulesLink()->toString(),
+        ]),
         '#required' => TRUE,
         '#default_value' => FALSE,
         '#weight' => 97,
@@ -40,6 +44,34 @@ class SubmissionForm extends ContentEntityForm {
     }
 
     return $form;
+  }
+
+  /**
+   * Build a "contest rules" link.
+   *
+   * @return \Drupal\Core\Link
+   *   A renderable link object.
+   */
+  protected function buildContestRulesLink() {
+    return Link::fromTextAndUrl(
+      $this->t('contest rules and conditions'),
+      Url::fromUri($this->getContestRulesUri(), [
+        'attributes' => [
+          'target' => '_blank',
+        ],
+      ])
+    );
+  }
+
+  /**
+   * Return the URI of the "Contest rules" page.
+   *
+   * @return string
+   *   A string.
+   */
+  protected function getContestRulesUri() {
+    $scheme_and_host = $this->getRequest()->getSchemeAndHttpHost();
+    return "{$scheme_and_host}/researchcommons/ior";
   }
 
 }
