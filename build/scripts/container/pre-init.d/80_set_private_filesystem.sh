b7be90e01373a25d03cf2325c4366a698bd6300e
Jacob Sanford <jsanford@unb.ca>
2021-12-02 10:24:40 -0400

    DOCKW-44 Migrate to upstream private filesystem deployment

--- build/scripts/container/pre-init.d/80_set_private_filesystem.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p ${DRUPAL_PRIVATE_FILE_PATH} && chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DRUPAL_PRIVATE_FILE_PATH}
-DRUSH_COMMAND="drush --root=${DRUPAL_ROOT} --uri=default --yes"
-$DRUSH_COMMAND eval 'use Drupal\Component\FileSecurity\FileSecurity; print FileSecurity::htaccessLines(FALSE)' > ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
-chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
