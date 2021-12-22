26fbd927e554531fa533610f127e00caf7fc662b
Patrick Schilf <pschilf@unb.ca>
2021-12-22 12:02:32 -0400

    LIB-485 Remove award type entity

--- custom/modules/ior/ior_awards/src/Form/AwardForm.php
+++ custom/modules/ior/ior_awards/src/Form/AwardForm.php
@@ -4,7 +4,6 @@ namespace Drupal\ior_awards\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\ior_awards\Entity\AwardTypeInterface;
 
 /**
  * Default form for IOR award entities.
@@ -22,34 +21,6 @@ class AwardForm extends ContentEntityForm {
         ->getParameter('ior_submission');
       $this->entity->setSubmission($submission);
     }
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
-
-    /** @var \Drupal\ior_awards\Entity\Storage\AwardTypeStorageInterface $storage */
-    $storage = $this->entityTypeManager->getStorage('ior_award_type');
-    $form['field_type']['widget']['#empty_option'] = $this->t('- Select a value -');
-    $form['field_type']['widget']['#options'] = array_map(function (AwardTypeInterface $award_type) {
-      return $award_type->label();
-    }, $storage->loadUnawarded($this->getContest()->id()));
-
-    return $form;
-  }
-
-  /**
-   * Get the contest.
-   *
-   * @return \Drupal\ior\Entity\ContestInterface
-   *   A contest entity.
-   */
-  protected function getContest() {
-    return $this->getEntity()
-      ->getSubmission()
-      ->getContest();
   }
 
 }
