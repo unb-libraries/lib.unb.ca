10f9a197e4ae983420128563a985cb36fd7a642b
Brian Cassidy <brian.cassidy@unb.ca>
2022-01-14 13:53:15 -0400

    LIB-444 js event handling change

--- custom/modules/eresources/js/forms.js
+++ custom/modules/eresources/js/forms.js
@@ -3,30 +3,30 @@
         attach: function(context, settings) {
             $('#eresources-reference-form .chosen-container, #eresources-databases-form .chosen-container').bind('keypress', function(e) {
               if(e.which === 13) {
-                var id = $(this).attr('id').replace('_chosen', '-submit').replace('_', '-');
+                var id = this.id.replace('_chosen', '-submit').replace('_', '-');
                 $('#' + id).click();
               }
             });
             $('#reference-guide-submit').click(function(e) {
-                e.preventDefault();
                 let cat = $('#reference-guide').val();
                 if (cat) {
+                    e.preventDefault();
                     window.location = 'https://guides.lib.unb.ca/category/' + cat;
                 }
             });
             $('#database-guide-submit').click(function(e) {
-                e.preventDefault();
                 let cat = $('#database-guide').val();
                 if (cat) {
+                    e.preventDefault();
                     window.location = 'https://guides.lib.unb.ca/category/' + cat;
                 }
             });
             $('#database-submit').click(function(e) {
-                e.preventDefault();
                 let query = $('#database').val();
                 if (query) {
-                  $('input[name=query]').val(query);
-                  $('#eresources-databases-form').submit();
+                    e.preventDefault();
+                    $('input[name=query]').val(query);
+                    $('#eresources-databases-form').submit();
                 }
             });
         }
