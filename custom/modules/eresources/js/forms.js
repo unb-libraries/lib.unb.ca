6a7afc685ca790eb9ad964cd095824b8ea71c47c
Brian Cassidy <brian.cassidy@unb.ca>
2022-08-05 13:56:08 -0300

    LIB-512 remove unneeded keypress event hooks

--- custom/modules/eresources/js/forms.js
+++ custom/modules/eresources/js/forms.js
@@ -1,12 +1,6 @@
 (function ($, Drupal) {
     Drupal.behaviors.eresourcesForms = {
         attach: function(context, settings) {
-            $('#eres-reference .chosen-container, #eres-databases .chosen-container').bind('keypress', function(e) {
-                if(e.which === 13) {
-                    var id = this.id.replace('_chosen', '-submit').replace('_', '-');
-                    $('#' + id).click();
-                }
-            });
             $('#reference-guide-submit').click(function(e) {
                 let cat = $('#reference-guide').val();
                 e.preventDefault();
@@ -27,12 +21,6 @@
                 if (query) {
                     $('input[name=query]').val(query);
                     $('#eres-databases').submit();
-                }
-            });
-            $('#eres-reference input[name=query], #eres-databases input[name=query]').on('keypress', function (e) {
-                if (e.keyCode == 13) {
-                    e.preventDefault();
-                    $(e.target).closest('form').find('input[type=submit]').last().focus().click();
                 }
             });
 
