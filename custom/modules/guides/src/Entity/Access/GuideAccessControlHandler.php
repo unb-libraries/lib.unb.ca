2f0e0f386886e69a2ac0bc5c070a3a7224853d34
Brian Cassidy <brian.cassidy@unb.ca>
2022-08-10 15:36:50 -0300

    LIB-517 add access handler for guide editing

---
+++ custom/modules/guides/src/Entity/Access/GuideAccessControlHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\guides\Entity\Access;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\custom_entity\Entity\Access\EntityAccessControlHandler;
+
+/**
+ * Access control handler for "guide" entities.
+ */
+class GuideAccessControlHandler extends EntityAccessControlHandler {
+
+  /**
+   * Grant editing access to any listed editor.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   An entity.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   A user.
+   *
+   * @return bool
+   *   TRUE if user is listed, FALSE otherwise.
+   */
+  protected function hasEntityUpdateAccess(EntityInterface $entity, AccountInterface $account) {
+    $hasAccess = FALSE;
+    foreach ($entity->editors as $editor) {
+      $user = $editor->entity->field_user->entity;
+      if ($user->id() == $account->id()) {
+        $hasAccess = TRUE;
+        break;
+      }
+    }
+    return $hasAccess;
+  }
+
+}
