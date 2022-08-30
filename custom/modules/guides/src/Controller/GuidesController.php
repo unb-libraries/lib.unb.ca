bb63d7b8794c4134809f07f1cbc206c9cfd31037
Brian Cassidy <brian.cassidy@unb.ca>
2022-08-30 14:50:39 -0300

    LIB-517 add category list for /guide page

--- custom/modules/guides/src/Controller/GuidesController.php
+++ custom/modules/guides/src/Controller/GuidesController.php
@@ -33,4 +33,28 @@ class GuidesController extends ControllerBase {
     return new RedirectResponse(Url::fromRoute('entity.guide.canonical', ['guide' => reset($ids)])->toString());
   }
 
+  /**
+   * List of categories for main guides page.
+   */
+  public function categories() {
+    $storage = $this->entityTypeManager()->getStorage('guide_category');
+    $query = $storage->getQuery();
+    $ids = $query
+      ->condition('status', 1)
+      ->sort('title', 'ASC')
+      ->execute();
+    $list = $storage->loadMultiple($ids);
+
+    return [
+      '#theme' => 'categories',
+      '#title' => 'Research by Subject',
+      '#categories' => $list,
+      '#cache' => [
+        'tags' => [
+          'guide_category_list',
+        ],
+      ],
+    ];
+  }
+
 }
