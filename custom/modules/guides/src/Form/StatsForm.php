92af45b9c94fe707d64583164d85bd4453072f7d
Brian Cassidy <brian.cassidy@unb.ca>
2022-09-20 16:12:10 -0300

    LIB-535 stats views for guides, categories and users

---
+++ custom/modules/guides/src/Form/StatsForm.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Drupal\guides\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\guides\Controller\GuidesStatsController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Generate CSV stats data.
+ */
+class StatsForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'guides_stats_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $start = '2017-09-01';
+    $today = date('Y-m-d');
+
+    $form['start_date'] = [
+      '#type' => 'date',
+      '#attributes' => ['type' => 'date', 'min' => $start, 'max' => $today],
+      '#default_value' => $start,
+      '#required' => TRUE,
+      '#title' => $this
+        ->t('Start Date'),
+    ];
+
+    $form['end_date'] = [
+      '#type' => 'date',
+      '#attributes' => ['type' => 'date', 'min' => $start, 'max' => $today],
+      '#default_value' => $today,
+      '#required' => TRUE,
+      '#title' => $this
+        ->t('End Date'),
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Generate CSV File'),
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $dates = [
+      $form_state->getValue('start_date'),
+      $form_state->getValue('end_date'),
+    ];
+    $dimensions = ['ga:pagePath', 'ga:yearMonth'];
+    $controller = GuidesStatsController::create(\Drupal::getContainer());
+    $route = $this->getRouteMatch();
+
+    $params = $route->getParameters()->all();
+    $object = reset($params);
+    $path = $object->toUrl()->toString();
+    $results = $controller->getStatsFor($path, $dates, $dimensions);
+
+    // Add tab names.
+    if ($guide = $route->getParameter('guide')) {
+      $tabs = [];
+      foreach ($guide->sections as $section) {
+        $entity = $section->entity;
+        $tabs[$entity->id()] = $entity->field_section_label->value;
+      }
+
+      $match = '/^' . preg_quote($path, '/') . '(?:#section-(\d+))?$/';
+      for ($i = 0; $i < count($results); $i++) {
+        if (preg_match($match, $results[$i]['ga:pagePath'], $matches)) {
+          if (!empty($matches[1]) && !empty($tabs[$matches[1]])) {
+            $results[$i]['ga:pagePath'] .= ' (' . $tabs[$matches[1]] . ')';
+          }
+        }
+      }
+    }
+
+    $rows = $this->mergeStatsRows($results);
+    $output = '';
+    foreach ($rows as $row) {
+      $output .= implode(',', $row) . "\n";
+    }
+
+    $response = new Response();
+    $response->setContent($output);
+    $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+    $response->headers->set('Content-Disposition', 'attachment; filename=stats.csv');
+
+    $form_state->setResponse($response);
+  }
+
+  /**
+   * Map rows of results to pageviews per month.
+   */
+  private function mergeStatsRows($results) {
+    $data = [];
+    $dates = [];
+    foreach ($results as $result) {
+      $data[$result['ga:pagePath']][$result['ga:yearMonth']] = $result['pageviews'];
+      $dates[$result['ga:yearMonth']] = 1;
+    }
+
+    ksort($dates);
+    ksort($data);
+
+    $rows = [array_merge(['Page'], array_keys($dates))];
+    foreach ($data as $page => $dateData) {
+      $row = [$page];
+      foreach (array_keys($dates) as $date) {
+        $row[] = empty($dateData[$date]) ? 0 : $dateData[$date];
+      }
+      $rows[] = $row;
+    }
+
+    return $rows;
+  }
+
+}
