8673f6a15fb04d35bf96adcc41ab94a034282080
Brian Cassidy <brian.cassidy@unb.ca>
2022-10-19 12:02:59 -0300

    LIB-534 use extra param for guide id; add cancel button

--- custom/modules/guides/src/Form/CourseLinkForm.php
+++ custom/modules/guides/src/Form/CourseLinkForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\guides\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Default form for "course_link" entities.
@@ -10,15 +11,59 @@ use Drupal\Core\Entity\ContentEntityForm;
 class CourseLinkForm extends ContentEntityForm {
 
   /**
+   * Guide ID.
+   *
+   * @var int
+   */
+  private $guide;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, int $guide = NULL) {
+    $this->guide = $guide;
+
+    $form = parent::buildForm($form, $form_state);
+
+    $form['actions']['cancel'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Cancel'),
+      '#weight' => 5,
+      '#submit' => ['::cancelForm'],
+      '#limit_validation_errors' => [],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Cancel the add/edit and return to course link list.
+   */
+  public function cancelForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect('entity.course_link.collection',
+      [
+        'guide' => $this->guide,
+      ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect('entity.course_link.collection',
+      [
+        'guide' => $this->guide,
+      ]);
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
    * Assign guide, if not already assigned.
    */
   protected function prepareEntity() {
     parent::prepareEntity();
     if (!$this->entity->guide->entity) {
-      $guide = $this
-        ->getRouteMatch()
-        ->getParameter('guide');
-      $this->entity->set('guide', $guide);
+      $this->entity->set('guide', $this->guide);
     }
   }
 
