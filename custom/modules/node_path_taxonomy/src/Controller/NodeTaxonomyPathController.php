4ce9b23d1336fe87cc6d2fda9cc77bd1a19ab0be
Brian Cassidy <brian.cassidy@unb.ca>
2022-10-27 15:12:02 -0300

    LIB-376 move node taxonomy path save to separate submit hook

--- custom/modules/node_path_taxonomy/src/Controller/NodeTaxonomyPathController.php
+++ custom/modules/node_path_taxonomy/src/Controller/NodeTaxonomyPathController.php
@@ -4,6 +4,7 @@ namespace Drupal\node_path_taxonomy\Controller;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node_path_taxonomy\Entity\NodeTaxonomyPath;
+use Drupal\node_path_taxonomy\Entity\NodeTaxonomyPathRelationship;
 
 /**
  * NodeTaxonomyPathController object.
@@ -21,6 +22,21 @@ class NodeTaxonomyPathController {
   public static function setPath(array &$form, FormStateInterface $form_state) {
     $path_tid = $form_state->getValue('publication_path');
     \Drupal::state()->set(NodeTaxonomyPath::getPathTaxonomyTidStateKey(), $path_tid);
+  }
+
+  /**
+   * Save the path tid from the form.
+   *
+   * @param string[] $form
+   *   The Drupal associative form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   */
+  public static function savePath(array &$form, FormStateInterface $form_state) {
+    $node = $form_state->getFormObject()->getEntity();
+    $path_tid = $form_state->getValue('publication_path');
+
+    NodeTaxonomyPathRelationship::setNodePathRelationship($node, $path_tid);
   }
 
 }
