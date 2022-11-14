1b7832cf3010dd65139a06df41bca5460c3eefb0
Patrick Schilf <pschilf@unb.ca>
2022-11-14 12:05:38 -0400

    Increase attempts, add delay to flaky eresources/KB test

--- tests/cypress/status/e2e/eresources-lib-unb-ca.cy.js
+++ tests/cypress/status/e2e/eresources-lib-unb-ca.cy.js
@@ -1,8 +1,10 @@
 const eresourcesBaseUrl = 'https://lib.unb.ca/eresources'
-describe('e-Resources', {baseUrl: eresourcesBaseUrl, groups: ['core','eresources']}, () => {
+describe('e-Resources', {baseUrl: eresourcesBaseUrl, retries: {runMode: 4}, groups: ['core','eresources']}, () => {
 
   context('Knowledge Base and Resolver', {baseUrl: eresourcesBaseUrl}, () => {
     specify('Searching "Journals & Newspapers" for "Biology" should find 10+ results', () => {
+      cy.waitAfterFailedAttempts(3, 60000)
+
       cy.visit('?form_id=eres_journals')
       cy.get('form#eres-journals').within(() => {
         cy.get('input#journals-query')
