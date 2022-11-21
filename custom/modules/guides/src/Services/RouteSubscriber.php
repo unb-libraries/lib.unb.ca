2bc0754c33098cb97a5ce2948780978e9101e09c
Brian Cassidy <brian.cassidy@unb.ca>
2022-11-21 15:40:28 -0400

    LIB-532 adjust permissions for guides profiles and use admin theme for editing

---
+++ custom/modules/guides/src/Services/RouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\guides\Services;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Use admin theme for profile editing.
+    if ($route = $collection->get('profile.user_page.single')) {
+      $route->setOption('_admin_route', TRUE);
+    }
+  }
+
+}
