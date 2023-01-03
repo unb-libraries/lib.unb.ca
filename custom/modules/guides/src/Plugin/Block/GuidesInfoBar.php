67e3a9dc10fbe8fd6f59fd1b4fdb3a7dc9761f33
Brian Cassidy <brian.cassidy@unb.ca>
2023-01-03 16:48:11 -0400

    LIB-548 more guide info bar

--- custom/modules/guides/src/Plugin/Block/GuidesInfoBar.php
+++ custom/modules/guides/src/Plugin/Block/GuidesInfoBar.php
@@ -3,6 +3,10 @@
 namespace Drupal\guides\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides an information bar for guides.
@@ -13,7 +17,44 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("UNB Libraries Guides"),
  * )
  */
-class GuidesInfoBar extends BlockBase {
+class GuidesInfoBar extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new CartBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -43,7 +84,23 @@ class GuidesInfoBar extends BlockBase {
         $status .= ' within ' . implode(' and ', $categories);
       }
 
-      // @todo Add course info part.
+      $storage = $this->entityTypeManager->getStorage('course_link');
+      $query = $storage->getQuery();
+      $ids = $query
+        ->condition('guide', $guide->id())
+        ->execute();
+
+      if (!empty($ids)) {
+        if (count($ids) > 1) {
+          $linkUrl = Url::fromRoute('entity.course_link.collection', ['guide' => $guide->id()])->toString();
+          $status .= ' AND matches several <a href="' . $linkUrl . '">specific D2L courses</a>';
+        }
+        else {
+          $course = $storage->loadMultiple(reset(ids));
+          // @todo Add course info.
+        }
+      }
+
       $build['#markup'] = '<p class="alert alert-info"><span class="fas fa-certificate"></span> ' . $status . '.</p>';
     }
 
