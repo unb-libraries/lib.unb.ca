0cbb00908fbd20cd10d4895365f0391f4de10ace
Patrick Schilf <pschilf@unb.ca>
2023-01-10 10:20:02 -0400

    LIB-566 Install submission type entity

---
+++ custom/modules/ior/src/Entity/SubmissionType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\ior\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Submission type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "ior_submission_type",
+ *   label = @Translation("Submission type"),
+ *   label_plural = @Translation("Submission types"),
+ *   label_collection = @Translation("Submission types"),
+ *   handlers = {
+ *     "route_provider" = {
+ *       "html" = "Drupal\custom_entity\Entity\Routing\HtmlRouteProvider",
+ *     },
+ *     "access" = "Drupal\custom_entity\Entity\Access\EntityAccessControlHandler",
+ *   },
+ *   config_prefix = "ior_submission_type",
+ *   bundle_of = "ior_submission",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *   }
+ * )
+ */
+class SubmissionType extends ConfigEntityBundleBase {
+
+}
