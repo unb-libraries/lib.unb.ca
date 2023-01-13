51a7a080b176b85098ebb54d7aabe100d67916cb
Patrick Schilf <pschilf@unb.ca>
2023-01-13 08:35:37 -0400

    LIB-566 Convert field_last_name -> last_name

--- custom/modules/ior/src/Entity/Submission.php
+++ custom/modules/ior/src/Entity/Submission.php
@@ -180,6 +180,15 @@ class Submission extends ContentEntityBase implements SubmissionInterface {
       ->setDisplayOptions('view', ['weight' => 10])
       ->setDisplayOptions('form', ['weight' => 10]);
 
+    $fields['last_name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Last name'))
+      ->setRequired(TRUE)
+      ->setRevisionable(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', ['weight' => 20])
+      ->setDisplayOptions('form', ['weight' => 20]);
+
     $fields += static::publishedBaseFieldDefinitions($entity_type);
     $fields[$entity_type->getKey('published')]
       ->setDefaultValue(FALSE);
