ce58c00204803d56b4e8b11b3e1037235a2869ee
Brian Cassidy <brian.cassidy@unb.ca>
2023-02-23 10:30:59 -0400

    LIB-560 override user toolbar links

---
+++ custom/modules/public_profile/src/Services/UserToolbarLinkBuilder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\public_profile\Services;
+
+use Drupal\user\ToolbarLinkBuilder;
+use Drupal\Core\Url;
+
+/**
+ * User toolbar link overrides.
+ */
+class UserToolbarLinkBuilder extends ToolbarLinkBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function renderToolbarLinks() {
+    $build = parent::renderToolbarLinks();
+
+    $build['#links']['account']['title'] = $this->t('View public profile');
+    $build['#links']['account']['url'] = Url::fromRoute('public_profile.view', [
+      'user' => $this->account->id(),
+    ]);
+
+    $build['#links']['account_edit']['title'] = $this->t('Edit public profile');
+    $build['#links']['account_edit']['url'] = Url::fromRoute('profile.user_page.single', [
+      'user' => $this->account->id(),
+      'profile_type' => 'public',
+    ]);
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    return ['renderToolbarLinks'];
+  }
+
+}
