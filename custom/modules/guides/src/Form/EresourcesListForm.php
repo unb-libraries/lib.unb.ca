38667970df13a98ec701a5d5ee126a4fe5f7b590
Brian Cassidy <brian.cassidy@unb.ca>
2023-08-01 09:33:56 -0300

    link kb record to canonical url (BugzID: 19464)

--- custom/modules/guides/src/Form/EresourcesListForm.php
+++ custom/modules/guides/src/Form/EresourcesListForm.php
@@ -200,8 +200,8 @@ class EresourcesListForm extends FormBase {
       else {
         $text .= '<p class="catalogue">This record is <b>maintained by Cataloguing</b> and is part of eResources Discovery.</p>';
         if ($this->currentUser()->hasPermission('update eresources_record entities')) {
-          $editUrl = Url::fromRoute('entity.eresources_record.edit_form', ['eresources_record' => $id])->toString();
-          $text .= '<a class="button" href="' . $editUrl . '">Edit Record</a>';
+          $recordUrl = Url::fromRoute('entity.eresources_record.canonical', ['eresources_record' => $id])->toString();
+          $text .= '<a class="button" href="' . $recordUrl . '">View Record</a>';
         }
       }
 
