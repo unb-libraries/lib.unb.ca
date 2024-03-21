5378c3db5ae35e9c290560eba4e858128c1ae10d
Patrick Schilf <pschilf@unb.ca>
2024-03-21 08:53:25 -0300

    Wrap IoR invite copy action in button

--- custom/modules/ior/js/invite.behaviors.js
+++ custom/modules/ior/js/invite.behaviors.js
@@ -1,11 +1,22 @@
 (function ($, Drupal) {
     Drupal.behaviors.invite = {
         attach(context, settings) {
-          $('#block-pagetitle .invite', context).each(function() {
+
+          $('#block-pagetitle .invite', context).each(function(index, btn) {
+              const originalText = btn.children[0].innerText
+              const setLabel = (label) => btn.children[0].innerText = label
+
               $(this).click(async function (event ) {
+                  event.preventDefault()
                   const { origin, pathname } = window.location
                   const url = `${origin}/saml/login?destination=${pathname}`
                   await navigator.clipboard.writeText(url)
+                  setLabel('URL copied')
+                  const listener = btn.addEventListener('blur', () => {
+                      setLabel(originalText)
+                      btn.removeEventListener('blur', listener)
+                  })
+
               })
           })
         },
