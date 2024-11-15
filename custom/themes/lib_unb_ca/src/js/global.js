223249efacbaffeb75a73e88a51f952086035186
Camilo Villamizar <cvillami@unb.ca>
2024-11-15 12:21:14 -0400

    LIB-667 Replace JS media-figure filter w/ hook

--- custom/themes/lib_unb_ca/src/js/global.js
+++ custom/themes/lib_unb_ca/src/js/global.js
@@ -12,13 +12,6 @@
               adjustAnchor();
             });
 
-             // Remove everthing but <img> and <figcaption> from Media figures
-            $('figure.media-figure').each(function() {
-              var img = $(this).find('img').first();
-              var caption = $(this).find('figcaption').first();
-              $(this).empty().append(img).append(caption);
-            });
-
             // Mobile Quicklinks button: scroll to Quicklinks section when toggled on.
             $('#btn-quicklinks').click(function(e) {
                 e.preventDefault();
