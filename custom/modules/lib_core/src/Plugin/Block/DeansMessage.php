d2317d44214fbb03dccf8829f9cc5193498a895f
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-11-25 13:42:06 -0400

    LIB-685 Switch Dean's Message block link from webform to page node

--- custom/modules/lib_core/src/Plugin/Block/DeansMessage.php
+++ custom/modules/lib_core/src/Plugin/Block/DeansMessage.php
@@ -38,7 +38,7 @@ class DeansMessage extends BlockBase {
   protected function getValue() {
     // Note: image manually resized to match Content Lg Breakpoint image style.
     $html = '
-      <a href="/oa-policy">
+      <a href="/openaccess/open-access-policy">
         <figure class="figcaption-overlay front-page-border">
             <img src="/modules/custom/lib_core/img/towards_open_update.jpg"
              alt="Towards Open: Learn more about UNB\'s newly-adopted Open Access Policy and how the Library can support your publishing.">
