e12bcc292e18f8c915ca97aabdd39fc87512e6a4
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2025-01-31 09:52:17 -0400

    LIB-694 Restore Oct-23 image/alt/figcaption, update link URL

--- custom/modules/lib_core/src/Plugin/Block/DeansMessage.php
+++ custom/modules/lib_core/src/Plugin/Block/DeansMessage.php
@@ -38,11 +38,11 @@ class DeansMessage extends BlockBase {
   protected function getValue() {
     // Note: image manually resized to match Content Lg Breakpoint image style.
     $html = '<div class="front-page-border">
-      <a href="/openaccess/open-access-policy">
+      <a href="/about/welcome-libraries">
         <figure class="figcaption-overlay">
-            <img src="/modules/custom/lib_core/img/towards_open_update.jpg"
-             alt="Towards Open: Learn more about UNB\'s newly-adopted Open Access Policy and how the Library can support your publishing.">
-            <figcaption class="caption-bottom caption-hover font-size-smaller">Review the new policy &raquo;</figcaption>
+            <img src="/modules/custom/lib_core/img/lesley-balcomm-welcome-unb-libraries.png"
+             alt="Welcome to UNB Libraries">
+            <figcaption class="caption-bottom caption-hover font-size-smaller">Welcome and FAQs &raquo;</figcaption>
         </figure>
       </a>
     </div>';
