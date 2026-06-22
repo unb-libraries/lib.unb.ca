4a34aede46befc2f5f0ef0bc78f5ffd8d2634385
Brian Cassidy <brian.cassidy@unb.ca>
2026-06-22 13:27:09 -0300

    redo eres search block for updated interface

--- custom/modules/lib_core/src/Plugin/Block/PermittedUseTool.php
+++ custom/modules/lib_core/src/Plugin/Block/PermittedUseTool.php
@@ -20,21 +20,34 @@ class PermittedUseTool extends BlockBase {
    */
   public function build() {
     $html = '
-      <form action="//web.lib.unb.ca/eresources/index.php" class="alert alert-warning my-3 theme-dark" method="get">
+      <form action="/eresources" class="alert alert-warning my-3 theme-dark" method="get">
+        <input type="hidden" name="type" value="title">
         <div class="form-row mb-2">
-          <input id="sub" name="sub" type="hidden" value="all">
-		      <input name="searchtype" type="hidden" value="every_word">
-		      <b class="text-dark">Check Permitted Use of Resource</b>
-		    </div>
-		    <div class="form-row mx-2">
+          <b class="text-dark">Check Permitted Use of Resource</b>
+	</div>
+	<div class="form-row mx-2">
           <div class="form-group mb-0">
             <label class="font-weight-bold mx-1 my-0 py-2" for="title">Resource Name</label>
           </div>
           <div class="col-lg-5 form-group">
-            <input class="form-control" id="title" name="resource_title" placeholder="enter database or journal name" type="text">
+            <input class="form-control" id="title" name="query" placeholder="enter resource name" type="text">
+            <div class="custom-control-wrapper-inline radio text-black mt-2">
+              <div class="custom-control custom-radio">
+                <input data-drupal-selector="search-type-title" type="radio" id="search-type-title" name="form_id" value="eres_databases" checked="checked" class="form-radio custom-control-input">
+                <label class="custom-control-label" for="search-type-database">Database</label>
+              </div>
+              <div class="custom-control custom-radio">
+                <input data-drupal-selector="search-type-journal" type="radio" id="search-type-journal" name="form_id" value="eres_journals" class="form-radio custom-control-input">
+                <label class="custom-control-label" for="search-type-journal">Journal/Newspaper</label>
+              </div>
+              <div class="custom-control custom-radio">
+                <input data-drupal-selector="search-type-ereference" type="radio" id="search-type-ereference" name="form_id" value="eres_reference" class="form-radio custom-control-input">
+                <label class="custom-control-label" for="search-type-ereference">e-Reference Material</label>
+              </div>
+            </div>
             <div class="d-flex flex-row mt-2">
               <div class="mr-2"><i aria-hidden="true" class="fas fa-gavel fa-flip-horizontal"></i></div>
-              <div><a href="//web.lib.unb.ca/eresources/permitteduse.php">Understanding the permitted use tool</a></div>
+              <div><a href="/eresources/permitted-use-licensed-content">Understanding the permitted use tool</a></div>
             </div>
           </div>
           <div class="col form-group">
